1f25958c8735ae45ef1511ccd5aa3e389b103db2
raghubarua <raghubarua@gmail.com>
2020-06-25 10:18:53 +0000

    adding vpc,ec2,alb,scaling,rds,sns

---
+++ ec2/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_key_pair" "mytest-key" {
+  key_name   = "my-test-terraform-key-new1"
+  public_key = "${file(var.my_public_key)}"
+}
+
+data "template_file" "init" {
+  template = "${file("${path.module}/userdata.tpl")}"
+}
+
+resource "aws_instance" "my-test-instance" {
+  count = 2
+  #ami                    = "${data.aws_ami.rhel.id}"
+  ami                    = "ami-09d95fab7fff3776c"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${aws_key_pair.mytest-key.id}"
+  vpc_security_group_ids = ["${var.security_group}"]
+  subnet_id              = "${element(var.subnets, count.index)}"
+  user_data              = "${data.template_file.init.rendered}"
+
+  tags = {
+    Name = "my-instance-${count.index + 1}"
+  }
+}
+
+resource "aws_ebs_volume" "my-test-ebs" {
+  count             = 2
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  size              = 1
+  type              = "gp2"
+}
+
+resource "aws_volume_attachment" "my-vol-attach" {
+  count        = 2
+  device_name  = "/dev/xvdh"
+  instance_id  = "${aws_instance.my-test-instance.*.id[count.index]}"
+  volume_id    = "${aws_ebs_volume.my-test-ebs.*.id[count.index]}"
+  force_detach = true
+}
---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source        = "./vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  public_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+module "ec2" {
+  source         = "./ec2"
+  my_public_key  = "/tmp/id_rsa.pub"
+  instance_type  = "t2.micro"
+  security_group = "${module.vpc.security_group}"
+  subnets        = "${module.vpc.public_subnets}"
+}
+
+module "alb" {
+  source = "./alb"
+  vpc_id = "${module.vpc.vpc_id}"
+
+  /*  instance1_id = "${module.ec2.instance1_id}"
+      instance2_id = "${module.ec2.instance2_id}"*/
+  subnet1 = "${module.vpc.subnet1}"
+
+  subnet2 = "${module.vpc.subnet2}"
+}
+
+module "auto_scaling" {
+  source           = "./auto_scaling"
+  vpc_id           = "${module.vpc.vpc_id}"
+  subnet1          = "${module.vpc.subnet1}"
+  subnet2          = "${module.vpc.subnet2}"
+  target_group_arn = "${module.alb.alb_target_group_arn}"
+}
+
+module "sns_topic" {
+  source       = "./sns"
+  alarms_email = "raghubarua@gmail.com"
+}
+
+module "cloudwatch" {
+  source      = "./cloudwatch"
+  sns_topic   = "${module.sns_topic.sns_arn}"
+  instance_id = "${module.ec2.instance_id}"
+}
+
